7e443093b6609e27d87070f05783dd491d27f5a2
TJ Gore <hello@tjwgore.com>
2024-10-18 12:56:28 -0400

    Add pokemon search and pokemon details card

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 Our goal is to fix and enhance a Pokedex application. If you are unfamiliar with the world of Pokemon, here is a brief explanation:
 
 > The Pokedex is an electronic device created and designed to catalog and provide information regarding the various species of Pokemon featured in the Pokemon video game, anime and manga series.
- 
+
 [Source](https://pokemon.fandom.com/wiki/Pokedex)
- 
+
 Our version of the Pokedex is able to list and search through Pokemon. However, our search is a bit buggy. Additionally, we want to add a feature that shows a selected Pokemon's details like its **type**, **moves**, and **evolution chain**.
 
 Your time is valuable, and we are extremely appreciative of you participating in this assessment. We're looking to gauge your ability to read and edit code, understand instructions, and deliver features, just as you would during your typical day-to-day work. We expect this test to take no more than one to two hours and ask to complete this work within the next two days. Upon submit, we will review and provide feedback to you regardless of our decision to continue the process.
@@ -19,9 +19,12 @@ When you are finished, please upload your completed work to your Github and invi
 ## Setup
 
 - This repo was scaffolded using `create-react-app`. As such, this app requires a stable version of `node` to get up and running.
+[[Updated]]
+ - If you have nvm, run `nvm use` to use the correct version of node. If not, install the correct version of node (20.18) to continue.
 - Clone this repo and run `npm install`.
 - To run the app, run `npm start`.
 - Please reach out to the Givebutter team if you have any issues with the initial setup or have any problems when running the initial app.
+
 
 ## Requirements
 
@@ -35,7 +38,7 @@ When you are finished, please upload your completed work to your Github and invi
 ![](mockup0.png)
 
 ### Details Card
-     
+
 - Clicking "Get Details" for any given Pokemon should render a card that has the Pokemon's `name`, `types`, `moves`, and `evolution chain`
 - Use the api functions defined in `api.js` to retrieve this data. Adding new endpoints or editing existing ones are out of scope
 - The details card should match the below mockup
@@ -48,6 +51,25 @@ Please take some time to answer the following questions. Your answers should go 
 
 - Given more time, what would you suggest for improving the performance of this app?
 
+  I would suggest the following improvements to enhance the performance of this app:
+  - Save the initial `fetchAllPokemon` API call data to local storage for a few days to prevent a request on the app's initial render. Pokemon data is small and unlikely to change often, so this will save a few API calls.
+  - Add react query and use its caching functionality to prefetch and store response data for `fetchPokemonSpeciesByName`, `fetchPokemonDetailsByName`, and `fetchEvolutionChainById` under the same key. This will reduce the number of API calls for Pokemon details that are viewed often.
+  - Break out the Pokemon list and card details into separate components and use react hook `useCallback` and `memo` to prevent rerendering the Pokemon list 151 times when clicking the "Get details" buttons for a Pokemon. This improvement will cause only the list item for the clicked button to rerender rather than the entire list.
+
 - Is there anything you would consider doing if we were to go live with this app?
 
+  I would consider the following before going live:
+    - Make the app responsive.
+    - Fix the glitch when clicking the "Get details" button. I would probably add the Pokemon details as a modal or keep the details card visible with a default Pokemon.
+    - Make the search accessible with the following: wrap the input in a form, add a search label, and add a aria-live region or alert role to announce the results and errors.
+    - Create the production build of the app.
+    - Add Cloudflare to cache JS and CSS assets and to protect against DDos attacks.
+    - Add analytics to track usage of searches and clicks. I would use a free Sprig account.
+    - Add Sentry.io for performance monitoring and error tracking.
+    - Create a minimal setup to deploy on push.
+    - Remove the request param `limit=151` and add pagination because we Gotta Catch ‘Em All!  [Pokemon Theme Song](https://www.youtube.com/watch?v=rg6CiPI6h2g)
+
+
 - What was the most challenging aspect of this work for you (if at all)?
+
+  It has been a long time since I have written CSS like this. I have been a hard-core Tailwind CSS user for a couple of years, and using BEM css naming again felt odd.
--- src/App.js
+++ src/App.js
@@ -1,70 +1,140 @@
-import { useEffect, useState } from "react";
-import { fetchAllPokemon } from "./api";
+import { useEffect, useState } from 'react';
+import { fetchAllPokemon, fetchPokemonDetailsByName, fetchEvolutionChainById, fetchPokemonSpeciesByName } from './api';
+
+const STATUS = {
+	idle: 'idle',
+	loading: 'loading',
+	success: 'success',
+	error: 'error',
+};
 
 function App() {
-    const [pokemonIndex, setPokemonIndex] = useState([])
-    const [pokemon, setPokemon] = useState([])
-    const [searchValue, setSearchValue] = useState('')
-    const [pokemonDetails, setPokemonDetails] = useState()
+	const [pokemonIndex, setPokemonIndex] = useState([]);
+	const [pokemon, setPokemon] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [pokemonDetails, setPokemonDetails] = useState(null);
+	const [status, setStatus] = useState(STATUS.idle);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const showPokemonDetails = status === STATUS.success && pokemon.length > 0 && pokemonDetails;
+	const showNoResults = status !== STATUS.error && pokemon.length === 0;
 
-    useEffect(() => {
-        const fetchPokemon = async () => {
-            const {results: pokemonList} = await fetchAllPokemon()
+	useEffect(() => {
+		const fetchPokemon = async () => {
+			try {
+				const { results: pokemonList } = await fetchAllPokemon();
+				setPokemon(pokemonList);
+				setPokemonIndex(pokemonList);
+			} catch (error) {
+				setStatus(STATUS.error);
+				setErrorMessage('An error occurred while fetching Pokemon. Try refreshing the page.');
+			}
+		};
+		fetchPokemon();
+	}, []);
 
-            setPokemon(pokemonList)
-            setPokemonIndex(pokemonList)
-        }
+	const onSearchValueChange = (event) => {
+		const value = event.target.value.toLowerCase();
+		setSearchValue(event.target.value);
+		setPokemon(pokemonIndex.filter((monster) => monster.name.toLowerCase().includes(value)));
+	};
 
-        fetchPokemon().then(() => {
-            /** noop **/
-        })
-    }, [searchValue])
+	const onGetDetails = (monsterName) => async () => {
+		try {
+			setStatus(STATUS.error);
+			setStatus(STATUS.loading);
+			const [pokemonDetails, pokemonSpecies] = await Promise.all([
+				fetchPokemonDetailsByName(monsterName),
+				fetchPokemonSpeciesByName(monsterName),
+			]);
 
-    const onSearchValueChange = (event) => {
-        const value = event.target.value
-        setSearchValue(value)
+			const monsterId = pokemonSpecies.evolution_chain?.url
+				.replace('https://pokeapi.co/api/v2/evolution-chain/', '')
+				.replace('/', '');
 
-        setPokemon(
-            pokemonIndex.filter(monster => !monster.name.includes(value))
-        )
-    }
+			const evolutionChain = await fetchEvolutionChainById(monsterId);
 
-    const onGetDetails = (name) => async () => {
-        /** code here **/
-    }
+			setPokemonDetails({
+				...pokemonDetails,
+				evolutionNames: getEvolutionNames(evolutionChain.chain),
+			});
+			setStatus(STATUS.success);
+		} catch (error) {
+			setStatus(STATUS.error);
+			setErrorMessage('An error occurred while fetching Pokemon details. Try refreshing the page.');
+		}
+	};
 
-    return (
-        <div className={'pokedex__container'}>
-            <div className={'pokedex__search-input'}>
-                <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
-            </div>
-            <div className={'pokedex__content'}>
-                {pokemon.length > 0 && (
-                    <div className={'pokedex__search-results'}>
-                        {
-                            pokemon.map(monster => {
-                                return (
-                                    <div className={'pokedex__list-item'} key={monster.name}>
-                                        <div>
-                                            {monster.name}
-                                        </div>
-                                        <button onClick={onGetDetails(monster.name)}>Get Details</button>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                )}
-                {
-                    pokemonDetails && (
-                        <div className={'pokedex__details'}>
-                            {/*  code here  */}
-                        </div>
-                    )
-                }
-            </div>
-        </div>
-    );
+	const getEvolutionNames = (chain, evolutionNames = []) => {
+		evolutionNames.push(chain.species.name);
+		if (chain.evolves_to.length === 0) {
+			return evolutionNames;
+		}
+		return getEvolutionNames(chain.evolves_to[0], evolutionNames);
+	};
+
+	return (
+		<div className={'pokedex__container'}>
+			{status === STATUS.loading && <p className={'pokedex__loading'}>Loading...</p>}
+			<div className={'pokedex__search-input'}>
+				<input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'} />
+			</div>
+			{status === STATUS.error && (
+				<div className={'pokedex__errors'}>
+					<p className={'pokedex__errors-small-margin'}>{errorMessage ?? 'Something went wrong. Please try again.'}</p>
+				</div>
+			)}
+			<div className={'pokedex__content'}>
+				{pokemon.length > 0 && (
+					<div className={'pokedex__search-results pokedex__content--rounded-corners pokedex__content--red-border'}>
+						{pokemon.map((monster) => {
+							return (
+								<div className={'pokedex__list-item'} key={monster.name}>
+									<div>{monster.name}</div>
+									<button onClick={onGetDetails(monster.name)}>Get Details</button>
+								</div>
+							);
+						})}
+					</div>
+				)}
+
+				{showNoResults && <p>No Results Found</p>}
+
+				{showPokemonDetails && (
+					<div className={'pokedex__details'}>
+						<div className={'pokedex__details-card pokedex__content--rounded-corners pokedex__content--black-border'}>
+							<p className={'pokedex__details-card--bold'}>{pokemonDetails.name}</p>
+							<div className={'pokedex__characteristics'}>
+								<div className={'pokedex__types'}>
+									<p className={'pokedex__details-card--bold'}>Types</p>
+									<ul>
+										{pokemonDetails.types?.map((typeItem) => (
+											<li key={typeItem.slot}>{typeItem.type.name}</li>
+										))}
+									</ul>
+								</div>
+								<div className={'pokedex__moves'}>
+									<p className={'pokedex__details-card--bold'}>Moves</p>
+									<ul>
+										{pokemonDetails.moves?.slice(0, 4).map((moveItem) => (
+											<li key={moveItem.move.name}>{moveItem.move.name}</li>
+										))}
+									</ul>
+								</div>
+							</div>
+							<div className={'pokedex__evolutions'}>
+								<p className={'pokedex__details-card--bold'}>Evolutions</p>
+								<ul className={'pokedex__evolutions--inline'}>
+									{pokemonDetails.evolutionNames?.slice(0, 4).map((name) => (
+										<li key={name}>{name}</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
